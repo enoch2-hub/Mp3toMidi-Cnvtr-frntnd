b61cef1b94f16a08d1f42e11be7a7c1481ad0719
enoch2-hub <enoch.perera.coding2@gmail.com>
2024-06-20 16:51:26 +0530

    add css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // src/App.js
 import React from 'react';
+import './App.css';
+
 import FileUpload from './components/FileUpload';
 
 function App() {
   return (
     <div className="App">
-      <h1>MP3 to MIDI Converter</h1>
-      <FileUpload />
+      <div className='container'>
+        <h1>MP3 to MIDI Converter</h1>
+          <FileUpload />
+        </div>
     </div>
   );
 }
